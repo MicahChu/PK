bd406f51410a07f52e4d4b9180e0bed0d283bb2f
MicahChu <maonline999@gmail.com>
2016-01-04 21:33:35 -0500

    Some patches

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
+{
+  var domain_string = valid_domain ? ("; domain=" + valid_domain) : '' ;
+  document.cookie = cookie_name + "=" + encodeURIComponent( cookie_value ) +
+      "; max-age=" + 60 * 60 * 24 * lifespan_in_days +
+      "; path=/" + domain_string ;
+}
+
+function get_cookie ( cookie_name )
+{
+  var cookie_string = document.cookie ;
+  if (cookie_string.length !== 0) {
+    var cookie_value = cookie_string.match ( '(^|;)[\s]*' + cookie_name + '=([^;]*)' );
+    return decodeURIComponent ( cookie_value[2] ) ;
+  }
+  return '' ;
+}
